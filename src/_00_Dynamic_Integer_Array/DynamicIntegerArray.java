1e3a50cb7ca2791c5d0d403d636315a698898246
league <league@wts20.attlocal.net>
2018-08-19 12:58:00 -0700

    Started

--- src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
+++ src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
@@ -2,22 +2,22 @@ package _00_Dynamic_Integer_Array;
 
 public class DynamicIntegerArray {
 	//1. Create a private int array. Don't initialize it.
-
+	private int[] ints;
 	
 	public DynamicIntegerArray() {
 		//2. Initialize the int array to have 0 elements. 
 		//   This will prevent a null pointer exception
-	
+		ints = new int[0];
 	}
 	
 	//3. Complete the steps in the add method
 	public void add(int v) {
 		//A. create and initialize a new int array to be one 
 		//   element longer than the member array
-		
+		int[] intnew = new int[ints.length+1];
 		//B. set the last element of your new array to 
 		//   the value passed into the method
-		
+		intnew[intnew.length-1] = v;
 		//C. iterate through the member array and 
 		//   copy every element from the member array 
 		//   to the new array
--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,6 +1,10 @@
 package _04_HangMan;
 
+import javax.swing.JOptionPane;
+
 public class HangMan{
+	
+	public int word = Integer.parseInt(JOptionPane.showInputDialog("Choose a number. Any number (Up to 266)"));
 	
 	
 	
